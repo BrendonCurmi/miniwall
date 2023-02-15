fed8134e4b7cb1b3cc91905d6d654adda358fbc0
BrendonCurmi <brendoncrm@gmail.com>
2023-02-16 00:24:16 +0100

    Create verifyToken jwt middleware

---
+++ middleware/verifyToken.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = async (req, res, next) => {
+    const token = req.header("Authorization");
+    if (token) {
+        try {
+            req.user = await jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+            next();
+        } catch (ex) {
+            res.sendStatus(403);
+        }
+    } else {
+        res.sendStatus(401);
+    }
+};
+
+module.exports = verifyToken;
