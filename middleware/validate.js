424164247e262dbc51b66db8f9733e556d8f63ab
BrendonCurmi <brendoncrm@gmail.com>
2023-02-15 01:05:18 +0100

    Add validate middleware for joi validation

---
+++ middleware/validate.js
@@ -0,0 +1,8 @@
+// Adapted from https://gist.github.com/stongo/6359042
+module.exports = (validator) => {
+    return (req, res, next) => {
+        const { error } = validator(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+        next();
+    }
+}
