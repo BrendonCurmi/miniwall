e6638e5e9da9ce6bdf6b0a46b7bd7834408bc93f
BrendonCurmi <brendoncrm@gmail.com>
2023-02-15 01:05:56 +0100

    Add validation middleware to userRoutes

--- users/userRoutes.js
+++ users/userRoutes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("./userController");
+const validateMiddleWare = require("../middleware/validate");
+const { validateUser } = require("./users.model");
 
-router.post("/signup", userController.createUser);
+router.post("/signup", [validateMiddleWare(validateUser)], userController.createUser);
 
 module.exports = router;
