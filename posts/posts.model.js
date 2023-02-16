6f53978c0b3aab7a87a219763a070948ec3d9a08
BrendonCurmi <brendoncrm@gmail.com>
2023-02-17 00:07:57 +0100

    Create posts model

---
+++ posts/posts.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+        required: true
+    },
+    timestamp: { type: Date, default: Date.now, required: true }
+});
+
+const PostTemplate = mongoose.model("PostTemplate", postSchema, "posts");
+
+const titleSchema = Joi.string().min(5).max(30);
+const contentSchema = Joi.string().min(5).max(200);
+const emailSchema = Joi.string().email();
+
+const validatePost = (user) => {
+    const schema = Joi.object({
+        title: titleSchema.required(),
+        content: contentSchema.required(),
+        email: emailSchema.required()
+    });
+    return schema.validate(user);
+};
+
+module.exports = {
+    PostTemplate,
+    validatePost
+};
