f4186eefde84692199d936c65a8b487a09dc86c8
BrendonCurmi <brendoncrm@gmail.com>
2023-02-16 22:21:40 +0100

    Use validateUpdateUser middleware for updating user

--- users/userRoutes.js
+++ users/userRoutes.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 const userController = require("./userController");
 const validateMiddleWare = require("../middleware/validate");
-const { validateUser, validateLogin } = require("./users.model");
+const { validateUser, validateUpdateUser, validateLogin } = require("./users.model");
 
 router.post("/signup", [validateMiddleWare(validateUser)], userController.createUser);
 
 router.route("/:username")
     .get(userController.getUser)
-    .put(validateMiddleWare(validateUser), userController.updateUser)
+    .put(validateMiddleWare(validateUpdateUser), userController.updateUser)
     .delete(userController.deleteUser);
 
 router.post("/login", [validateMiddleWare(validateLogin)], userController.login);
