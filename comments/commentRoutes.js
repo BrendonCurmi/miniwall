0dbb74584cfa1ef88a6f7cba7061871d0d047c09
BrendonCurmi <brendoncrm@gmail.com>
2023-02-21 17:49:00 +0100

    Create commentRoute

---
+++ comments/commentRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const commentController = require("./commentController");
+const validateMiddleWare = require("../middleware/validate");
+const { validateComment } = require("./comments.model");
+
+router.route("/post/:postId/comment").post([validateMiddleWare(validateComment)], commentController.addComment);
+
+router.route("/post/:postId/comments").get(commentController.getCommentsFromPost);
+
+router.route("/post/:postId/comment/:commentId")
+    .get(commentController.getComment)
+    .put(commentController.updateComment)
+    .delete(commentController.deleteComment);
+
+module.exports = router;
