a5f11d62e0eededb3bed82e32945019eb3c7b106
BrendonCurmi <brendoncrm@gmail.com>
2023-02-21 23:37:14 +0100

    Use userId from jwt to validate user in likeController

--- likes/likeController.js
+++ likes/likeController.js
@@ -1,18 +1,14 @@
 const { LikeTemplate } = require("./likes.model");
-const { UserTemplate } = require("../users/users.model");
 const { PostTemplate } = require("../posts/posts.model");
 
 exports.addLike = async (req, res) => {
-    const user = await UserTemplate.findOne({ email: req.body.email });
-    if (!user) return res.status(400).json({ message: "User does not exist" });
-
     const post = await PostTemplate.findOne({ _id: req.params.postId });
     if (!post) return res.status(400).json({ message: "Post does not exist" });
 
     try {
         const newLike = await new LikeTemplate({
             post_id: post._id,
-            owner_id: user._id
+            owner_id: req.decoded.userId
         }).save();
 
         await PostTemplate.findByIdAndUpdate(req.params.postId,
