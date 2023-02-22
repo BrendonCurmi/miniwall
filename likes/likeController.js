aac7abce881866d1985c6500a4f1c117d0f724bb
BrendonCurmi <brendoncrm@gmail.com>
2023-02-22 18:06:07 +0100

    Prevent users from deleting others likes

--- likes/likeController.js
+++ likes/likeController.js
@@ -40,6 +40,7 @@ exports.getLike = (req, res) => {
         .catch(err => res.status(500).json({ message: err.message }));
 };
 
+//todo this is unneeded. to be removed
 exports.updateLike = (req, res) => {
     LikeTemplate.updateOne({ _id: req.params.likeId }, req.body)
         .then(() => res.status(200).json({ ok: true }))
@@ -52,6 +53,10 @@ exports.deleteLike = async (req, res) => {
 
     const post = await PostTemplate.findById(req.params.postId );
     if (!post) return res.status(400).json({ message: "Post does not exist" });
+
+    if (!like.owner_id.equals(req.decoded.userId)) {
+        return res.status(400).json({ message: "Like can only be deleted by like owner" });
+    }
 
     await PostTemplate.findByIdAndUpdate(req.params.postId,
         {
