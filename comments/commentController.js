42983b7a3967c71f061d59660a3152095e7a7be0
BrendonCurmi <brendoncrm@gmail.com>
2023-02-25 14:16:17 +0100

    Update error code from 400 to 500 for updateComment

--- comments/commentController.js
+++ comments/commentController.js
@@ -62,7 +62,7 @@ exports.updateComment = async (req, res) => {
         CommentTemplate.findByIdAndUpdate(req.params.commentId, req.body);
         res.status(200).json({ ok: true });
     } catch (err) {
-        res.status(400).json({ message: err.message })
+        res.status(500).json({ message: err.message })
     }
 };
 
