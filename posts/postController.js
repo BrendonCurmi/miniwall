2d878bad9f44e48979bc48f70f8b45f153144c22
BrendonCurmi <brendoncrm@gmail.com>
2023-03-18 20:15:01 +0100

    Update updatePost return status

--- posts/postController.js
+++ posts/postController.js
@@ -37,8 +37,8 @@ exports.updatePost = async (req, res) => {
     }
 
     try {
-        PostTemplate.findByIdAndUpdate(req.params.postId, req.body);
-        res.status(200).json({ ok: true });
+        const updatedPost = await PostTemplate.findByIdAndUpdate(req.params.postId, req.body);
+        res.status(200).json(updatedPost);
     } catch (err) {
         res.status(500).json({ message: err.message })
     }
