9b383273690b6736d7a92cb26429d1fa78d2d3dc
BrendonCurmi <brendoncrm@gmail.com>
2023-03-18 20:14:16 +0100

    Update updateComment return status

--- comments/commentController.js
+++ comments/commentController.js
@@ -59,8 +59,8 @@ exports.updateComment = async (req, res) => {
     }
 
     try {
-        CommentTemplate.findByIdAndUpdate(req.params.commentId, req.body);
-        res.status(200).json({ ok: true });
+        const updatedComment = await CommentTemplate.findByIdAndUpdate(req.params.commentId, req.body);
+        res.status(200).json(updatedComment);
     } catch (err) {
         res.status(500).json({ message: err.message })
     }
