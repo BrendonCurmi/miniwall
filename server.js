f2fda7e4a0bc15dee2d360481fde2ae14188d6a5
BrendonCurmi <brendoncrm@gmail.com>
2023-03-23 17:08:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
+// Init environment variables
 require("dotenv").config();
 
+// Parses cookies
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
@@ -12,8 +14,7 @@ app.use(express.json());
 let xss = require("xss-clean");
 app.use(xss())
 
-// app.use(express.urlencoded({ extended: false }));
-
+// Add auth routes
 const authRouter = require("./users/authRoutes");
 app.use("/", authRouter);
 
@@ -21,6 +22,7 @@ app.use("/", authRouter);
 const verifyToken = require("./middleware/verifyToken");
 app.use(verifyToken);
 
+// Add routes
 const usersRouter = require("./users/userRoutes");
 app.use("/user", usersRouter);
 
@@ -38,7 +40,8 @@ const mongoose = require("mongoose");
 // Only insert fields to db if they are specified in schema
 mongoose.set("strictQuery", true);
 
-mongoose.connect(process.env.DATABASE_URI, () => console.log("Db connected"));
+// Connect to MongoDB
+mongoose.connect(process.env.DATABASE_URI, () => console.log("Database connected"));
 
 const port = process.env.SERVER_PORT
 app.listen(port, () => console.log("Server is up"));
