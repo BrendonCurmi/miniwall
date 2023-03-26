1b641cacc9e1d5b65bfc7c8865e1313c53221b0e
BrendonCurmi <brendoncrm@gmail.com>
2023-03-26 13:10:16 +0200

    Add max limit optional query parameter to getPosts

--- posts/postController.js
+++ posts/postController.js
@@ -14,7 +14,8 @@ exports.createPost = async (req, res) => {
 };
 
 exports.getPosts = (req, res) => {
-    PostTemplate.find().limit(20)
+    const limit = req.query.max || 30;
+    PostTemplate.find().limit(limit)
         .then(data => res.status(200).json(data))
         .catch(err => res.status(500).json({ message: err.message }));
 };
