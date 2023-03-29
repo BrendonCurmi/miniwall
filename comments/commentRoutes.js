19f62e328279565a12a6fd3c68d0cf253b8445ca
BrendonCurmi <brendoncrm@gmail.com>
2023-03-29 20:56:28 +0200

    Update

--- comments/commentRoutes.js
+++ comments/commentRoutes.js
@@ -5,9 +5,9 @@ const commentController = require("./commentController");
 const validateMiddleWare = require("../middleware/validate");
 const { validateComment } = require("./comments.model");
 
-router.route("/post/:postId/comment").post([validateMiddleWare(validateComment)], commentController.addComment);
-
 router.route("/post/:postId/comments").get(commentController.getCommentsFromPost);
+
+router.route("/post/:postId/comment").post([validateMiddleWare(validateComment)], commentController.addComment);
 
 router.route("/post/:postId/comment/:commentId")
     .get(commentController.getComment)
